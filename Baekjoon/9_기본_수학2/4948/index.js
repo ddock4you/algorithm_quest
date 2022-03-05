7e45bb14dba3affb60d09bc55fa7b659219ed956
ddock4you <ddock4you@gmail.com>
2022-03-05 18:29:48 +0900

    백준 알고리즘 문제 풀이

---
+++ Baekjoon/9_기본_수학2/4948/index.js
@@ -0,0 +1,57 @@
+// todo 제목: 베르트랑 공준
+// ! 문제: 베르트랑 공준은 임의의 자연수 n에 대하여, n보다 크고, 2n보다 작거나 같은 소수는 적어도 하나 존재한다는 내용을 담고 있다.
+// ! 이 명제는 조제프 베르트랑이 1845년에 추측했고, 파프누티 체비쇼프가 1850년에 증명했다.
+// ! 예를 들어, 10보다 크고, 20보다 작거나 같은 소수는 4개가 있다. (11, 13, 17, 19) 또, 14보다 크고, 28보다 작거나 같은 소수는 3개가 있다. (17,19, 23)
+// ! 자연수 n이 주어졌을 때, n보다 크고, 2n보다 작거나 같은 소수의 개수를 구하는 프로그램을 작성하시오.
+// * 입력: 입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 케이스는 n을 포함하는 한 줄로 이루어져 있다.
+// * 입력의 마지막에는 0이 주어진다.
+// ? 출력: 각 테스트 케이스에 대해서, n보다 크고, 2n보다 작거나 같은 소수의 개수를 출력한다.
+
+// * 1 ≤ n ≤ 123,456
+
+/*
+ *예제 출력
+     3
+     5
+     7
+     11
+     13
+ */
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+    .readFileSync(filePath)
+    .toString()
+    .split("\n")
+    .map((v) => Number(v));
+
+const array = input;
+const condition = (v) => v >= 1 && v <= 123456;
+const isPrime = (num) => {
+    if (num === 1) return;
+    for (let i = 2; i <= Math.sqrt(num); i += 1) {
+        if (num % i === 0) return;
+    }
+    return true;
+}
+array.forEach(v => {
+    let answer = [];
+    if (condition(v)) {
+        const n2 = v * 2;
+        for (let i = v + 1; i <= n2; i += 1) {
+            isPrime(i) ? answer.push(i) : null;
+        }
+        console.log(answer.length);
+    }
+})
+// if (condition(v)) {
+//     const isPrime = (num) => {
+//         if (num === 1) return;
+//         for (var i = 2; i <= Math.sqrt(num); i += 1) {
+//             if (num % i == 0) return;
+//         }
+//         return true;
+//     };
+//     for (var i = M; i <= N; i += 1) isPrime(i) ? console.log(i) : null;
+// }
