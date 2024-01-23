7ec9771d14dc128cb792e212036861baa7d1b32d
ddock4you <ddock4you@gmail.com>
2024-01-23 23:54:40 +0900

    백준 문제풀이 24262

---
+++ Baekjoon/시간 복잡도/24262/index.js
@@ -0,0 +1,11 @@
+/*
+TODO 제목: 알고리즘 수업 - 알고리즘의 수행 시간 1
+! https://www.acmicpc.net/problem/24262
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(1);
+console.log(0);
