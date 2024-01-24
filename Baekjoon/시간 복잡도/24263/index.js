e7f4288032f5f8ef104a7f1153f8cd7b8d9fc1ca
ddock4you <ddock4you@gmail.com>
2024-01-24 21:30:16 +0900

    백준 문제풀이 24264

--- Baekjoon/시간 복잡도/24263/index.js
+++ Baekjoon/시간 복잡도/24263/index.js
@@ -1,6 +1,6 @@
 /*
-TODO 제목: 알고리즘 수업 - 알고리즘의 수행 시간 1
-! https://www.acmicpc.net/problem/24262
+TODO 제목: 알고리즘 수업 - 알고리즘의 수행 시간 2
+! https://www.acmicpc.net/problem/24263
 */
 
 const fs = require("fs");
