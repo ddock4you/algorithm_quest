ff1c614dcfd8efae750e1cd16284945b79c9465e
ddock4you <ddock4you@gmail.com>
2024-02-17 22:38:17 +0900

    백준 문제풀이 18870

--- Baekjoon/정렬/18870/index.js
+++ Baekjoon/정렬/18870/index.js
@@ -5,19 +5,30 @@ TODO 제목: 좌표 압축
 
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs
-    .readFileSync(filePath)
-    .toString()
-    .trim()
-    .split("\n")
-    .slice(1)
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let result = '';
-const array = input[0].split(" ").map(v => +v);
-const sortArray = Array.from(new Set(array.sort((a,b) => b - a)));
-sortArray.forEach((v, i) => console.log(sortArray.slice(i + 1)))
+let [n, x] = input;
+x = x.split(" ").map((v) => +v);
+const sorted = Array.from(new Set([...x])).sort((a, b) => b - a);
+const extractArray = sorted.reduce((acc, value, i) => {
+  acc[value] = sorted.length - i - 1;
+  return acc;
+}, {});
+
+let result = [];
+// const result = [];
+
+x.forEach(
+  (value) =>
+    extractArray[value]
+      ? (result += `${extractArray[value]} `)
+      : (result += `0 `)
+  //   extractArray[value] ? result.push(extractArray[value]) : result.push(0)
+);
+
+console.log(result);
+// console.log(result.join(" "));
+
 // input.sort((a, b) => a[0] - b[0]).map((array) => console.log(`${array[0]} ${array[1]}`));
 
-
-
-// localeCompare: 두 문자열을 유니코드로 변환 후 결과를 반환해주는 메서드(앞의 문자열이 뒤의 문자열보다 숫자가 큰지 작은지 비교) 
+// localeCompare: 두 문자열을 유니코드로 변환 후 결과를 반환해주는 메서드(앞의 문자열이 뒤의 문자열보다 숫자가 큰지 작은지 비교)
