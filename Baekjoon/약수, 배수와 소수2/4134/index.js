a8097b200335c7e7cdf4e5d6616bca4035dd4d26
ddock4you <ddock4you@codeline.kr>
2024-02-29 12:53:11 +0900

    백준 문제풀이 4134

--- Baekjoon/약수, 배수와 소수2/4134/index.js
+++ Baekjoon/약수, 배수와 소수2/4134/index.js
@@ -10,47 +10,28 @@ const input = fs
   .toString()
   .trim()
   .split("\n")
-  .map((v) => v.split(" ").map((n) => +n))
+  .map((v) => +v)
   .slice(1);
-// const [a, b] = input;
-const gcd = (a, b) => {
-  while (b !== 0) {
-    let rest = a % b;
-    [a, b] = [b, rest];
+
+// 소수 판별 함수
+function isPrime(n) {
+  if (n < 2) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  // 3부터 n의 제곱근까지 홀수로 나누어보며 약수가 있는지 확인
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
   }
-  return a;
-};
+  // 약수가 없으면 소수
+  return true;
+}  
 
-// 가로수 사이의 거리
-const distance = [];
-for (let i = 0; i < input.length - 1; i++) {
-  distance.push(input[i + 1] - input[i]);
-}
-
-// 가로수 사이의 거리 최대공약수
-// distgcd 변수 가로수사이의 거리를 구하기 위해 첫 번째 가로수 사이로 변수 설정
-let distgcd = distance[0];
-for (let i = 0; i < distance.length; i++) {
-  distgcd = gcd(distgcd, distance[i]);
-}
-
-// 가로수 사이에 추가되는 부분 계산
-let result = 0;
-for (let i = 0; i < distance.length; i++) {
-  // 가로수 사이의 거리가 최대공약수랑 같지 않은 것만 계산
-  if (distance[i] > distgcd) {
-    result += distance[i] / distgcd - 1;
+input.forEach((number) => {
+  let result = number;
+  while (!isPrime(result)) {
+    result += 1;
   }
-}
+  console.log(result);
+})
 
-console.log(result);
-
-// 1. 간격의 최대공약수:
-// 문제에서 주어진 가로수들의 위치는 양의 정수로 주어집니다.
-// 가로수들의 간격을 최대공약수로 나누면, 모든 가로수들의 간격이 같아집니다.
-// 따라서 최대공약수를 구하는 것이 중요합니다.
-// 2. 새로 심어야 하는 가로수의 최소수:
-// 가로수들의 간격을 최대공약수로 나눈 값에서 1을 뺀 값을 모두 더하면, 새로 심어야 하는 가로수의 최소수를 구할 수 있습니다.
-// 이는 기존 가로수들 사이에만 새로운 가로수를 심을 수 있기 때문입니다.
-// 예를 들어, 가로수가 (1, 3, 7, 13)의 위치에 있다면 (5, 9, 11)의 위치에 가로수를 더 심으면 모든 가로수들의 간격이 같아집니다.
-// 따라서 새로 심어야 하는 가로수의 최소수는 3입니다.
+  // console.log(input);
